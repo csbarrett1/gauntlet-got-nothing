96c71af5c28e4ae588f3784b35a676bfd6426edb
Callan Morrison <callan.l.morrison@gmail.com>
2016-04-23 09:54:19 -0500

    updated weapons class

--- javascripts/enemies.js
+++ javascripts/enemies.js
@@ -7,7 +7,7 @@ let $ = require("jquery"),
     // enemies = require("./enemies.js"),
     player = require("./player.js");
     // spells = require("./spells.js"),
-    // weapons = require("./weapons.js");
+    weapons = require("./weapons.js");
 
 //To Do: Could add more enemy "species"
 
@@ -27,6 +27,17 @@ var Orc = function() {
 
     // Composes the corresponding player class into the player object
     this.class = new classes.GuildHall[randomClass]();
+    return this.class;
+
+  this.generateWeapon = function() {
+    // Get a random index from the allowed classes array
+    var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
+
+    // Get the string at the index
+    var randomClass = this.allowedClasses[random];
+
+    // Composes the corresponding player class into the player object
+    this.class = new weapons.GuildHall[randomClass]();
     return this.class;
   };
 };
