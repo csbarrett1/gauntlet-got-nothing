412e0fd70b23536eed119ac5341a55d3d4abd111
Callan Morrison <callan.l.morrison@gmail.com>
2016-04-23 14:37:15 -0500

    added event listener boilerplate

---
+++ javascripts/eventListeners.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//To-do: Make sure to list dependencies here 
+let $ = require("jquery");
+    // attack = require("./attack.js"),
+    // classes = require("./classes.js"),
+    // enemies = require("./enemies.js"),
+    // player = require("./player.js"),
+    // spells = require("./spells.js"),
+    // weapons = require("./weapons.js");
+    
+//Grabs pages (cards) from index.html
+let selectNamePage = $("#player-setup");
+let selectClassPage = $("#class-select");
+let selectWeaponPage = $("#weapon-select");
+let battlegroundPage = $("#battleground");
+let winPage = $("#win-page");
+let losePage = $("#losePage");
+
+var executeEventListeners = function() {
+
+
+
+
+
+
+
+}
+
+
+module.exports = {
+  executeEventListeners
+};
